c568591ffe2b9188e5c3e31b8d88ad27c150be55
Charanaraj N <goudacharanraj760@gmail.com>
2023-02-11 17:08:56 +0530

    feat: Header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <div className="app">
+        Charan
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import "./header.css"
+
+const Header = () => {
+  return <span className='header'> 🎞️ Little Media</span>
+}
+
+export default Header
