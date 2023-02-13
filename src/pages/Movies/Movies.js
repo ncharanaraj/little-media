ce250b8ea155c2021a1773e643d3d8eea5ebe0d0
Charanaraj N <goudacharanraj760@gmail.com>
2023-02-13 16:38:06 +0530

    movies page

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,7 +1,49 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 const Movies = () => {
-  return <span className="page-title">Movies</span>;
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [noOfPages, setNoOfPages] = useState();
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      ` https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+    );
+
+    console.log(data);
+    setContent(data.results);
+    setNoOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <span className="pageTitle">Movies</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.name || c.title}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      {noOfPages > 1 && (
+        <CustomPagination setPage={setPage} noOfPages={noOfPages} />
+      )}
+    </div>
+  );
 };
 
 export default Movies;
--- src/pages/Trending/Trending.js
+++ src/pages/Trending/Trending.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import CustomPagination from "../../components/Pagination/CustomPagination";
 import SingleContent from "../../components/SingleContent/SingleContent";
-import './Trending.css'
+import "./Trending.css";
 
 const Trending = () => {
+  const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
 
   const fetchTrending = async () => {
     const { data } =
-      await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}
+      await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}
     `);
 
     setContent(data.results);
@@ -16,7 +18,8 @@ const Trending = () => {
 
   useEffect(() => {
     fetchTrending();
-  }, []);
+    //eslint-disable-next-line
+  }, [page]);
 
   return (
     <div>
@@ -35,6 +38,7 @@ const Trending = () => {
             />
           ))}
       </div>
+      <CustomPagination setPage={setPage} />
     </div>
   );
 };
