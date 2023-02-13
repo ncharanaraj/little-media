36128874bf689a9ba1336d2228746a7b2f124bf2
Charanaraj N <goudacharanraj760@gmail.com>
2023-02-13 12:25:52 +0530

    API

---
+++ src/components/SingleContent/SingleContent.js
@@ -0,0 +1,12 @@
+const SingleContent = ({
+  id,
+  poster,
+  title,
+  date,
+  media_type,
+  vote_average,
+}) => {
+  return <div>{title}</div>;
+};
+
+export default SingleContent;
--- src/pages/Trending/Trending.js
+++ src/pages/Trending/Trending.js
@@ -1,8 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 const Trending = () => {
-  return (
-    <span className='page-title'>Trending</span>
-  )
-}
+  const [content, setContent] = useState([]);
 
-export default Trending
+  const fetchTrending = async () => {
+    const { data } =
+      await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}
+    `);
+
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
+  return (
+    <div>
+      <span className="pageTitle">Trending</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.name || c.title}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
